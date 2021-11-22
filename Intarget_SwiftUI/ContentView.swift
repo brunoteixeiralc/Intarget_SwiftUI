f8683df6eb47fdfd3aa36727eccde9819312fb9c
Bruno Correa <bruno.correa@sicoob.com.br>
2021-11-22 15:23:16 -0300

    Initial Commit

---
+++ Intarget_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Intarget_SwiftUI
+//
+//  Created by Bruno Teixeira Lemgruber Correa on 22/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Intarget_SwiftUI/Intarget_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intarget_SwiftUIApp.swift
+//  Intarget_SwiftUI
+//
+//  Created by Bruno Teixeira Lemgruber Correa on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Intarget_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
