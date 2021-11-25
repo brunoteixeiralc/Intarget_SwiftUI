5b23042346216b49bd170864e0f3b2c361cc8145
Bruno Correa <bruno.correa@sicoob.com.br>
2021-11-25 18:23:19 -0300

    mvp using swiftUI

---
+++ Intarget_SwiftUI/Views/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Intarget_SwiftUI
+//
+//  Created by Bruno Teixeira Lemgruber Correa on 22/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var alertIsVisible : Bool = false
+    @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
+    
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLEYES AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text(String(game.target))
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: $sliderValue, in: 1...100.0)
+                Text("100")
+                    .bold()
+            }
+            Button(action: {
+                alertIsVisible = true
+            }) {
+                Text("Hit me")
+            }
+            .alert(isPresented: $alertIsVisible) {
+                let roundedValue: Int = Int(sliderValue.rounded())
+                return Alert(title: Text("Hello There!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!!"))
+                )
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
